7288d52ba851d0ebb01bc7ba1d4a26d6693775d1
mebuki <mbking0808@gmail.com>
2020-09-10 15:18:37 +0900

    active_hashの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,24 @@ class ItemsController < ApplicationController
 
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :info, :category_id, :status_id, :fee_id, :place_id, :day_id, :price)
+  end
   
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,6 @@ class Item < ApplicationRecord
   belongs_to_active_hash :status
 
   validates :name, :info, :category, :status, :fee, :place, :day, :price, presence: true
-  validates :category, :status, :fee, :place, :day, numericality: { other_than: 0}
-  validates :price, format: { with:　/\A[0-9]+\z/i }, :numericality: { greater_than: 300, less_than: 9999999}
+  validates :category, :status, :fee, :place, :day, numericality: { other_than: 0 }
+  validates :price, format: { with: /\A[0-9]+\z/i }, inclusion: { in: 300..9_999_999 } 
 end
