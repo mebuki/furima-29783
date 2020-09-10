1fc6ec937ec95f66c83e53baac0add13690128e6
mebuki <mbking0808@gmail.com>
2020-09-10 15:29:20 +0900

    active_storageの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,8 +5,10 @@ class Item < ApplicationRecord
   belongs_to_active_hash :fee
   belongs_to_active_hash :place
   belongs_to_active_hash :status
+  has_one_attached :image
 
   validates :name, :info, :category, :status, :fee, :place, :day, :price, presence: true
   validates :category, :status, :fee, :place, :day, numericality: { other_than: 0 }
   validates :price, format: { with: /\A[0-9]+\z/i }, inclusion: { in: 300..9_999_999 } 
+  validates :image, presence: true
 end
