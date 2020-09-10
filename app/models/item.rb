4b9c8428a7d617814a34b933c8e6744440e70728
mebuki <mbking0808@gmail.com>
2020-09-10 12:29:18 +0900

    アソシエーションとバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :day
+  belongs_to_active_hash :fee
+  belongs_to_active_hash :place
+  belongs_to_active_hash :status
+
+  validates :name, :info, :category, :status, :fee, :place, :day, :price, presence: true
+  validates :category, :status, :fee, :place, :day, numericality: { other_than: 0}
+  validates :price, format: { with:　/\A[0-9]+\z/i }, :numericality: { greater_than: 300, less_than: 9999999}
 end
