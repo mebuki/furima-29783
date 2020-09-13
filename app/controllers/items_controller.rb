c5bd140162a9f7963c7176ff5740eef371139f5e
mebuki <mbking0808@gmail.com>
2020-09-13 18:29:44 +0900

    エラーメッセージの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
 
 
   def index
@@ -13,8 +13,9 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    # binding.pry
+    # item = Item.find(params[:id])
+    if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
