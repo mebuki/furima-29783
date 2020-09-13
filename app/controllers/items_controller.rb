95ac55007757844f9fd8b2bcf0bb585e47c4fe8e
mebuki <mbking0808@gmail.com>
2020-09-13 16:59:10 +0900

    編集を保存する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,6 +12,14 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
 
   def new
     @item = Item.new
