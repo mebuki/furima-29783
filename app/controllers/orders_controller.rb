85e93f5cdaaf72bd4759a6c8b9e2497f94092277
mebuki <mbking0808@gmail.com>
2020-09-14 15:42:05 +0900

    クレジットカード機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,28 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(order_params[:item_id])
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.permit(:token)
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_38302a33701caad1eed24042"
+    Payjp::Charge.create(
+      card: order_params[:token],
+      currency:'jpy'
+    )
   end
 
 end
